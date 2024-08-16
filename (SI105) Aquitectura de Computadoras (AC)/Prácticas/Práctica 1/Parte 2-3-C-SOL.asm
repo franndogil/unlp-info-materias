b596e9aaa55bdc2a5e53175ac153fb34504fc677
frandogil <franndogil@gmail.com>
2024-08-16 15:41:35 -0300

    ‎ ‎ ‎ ‎ ‎

---
+++ (SI105) Aquitectura de Computadoras (AC)/Prácticas/Práctica 1/Parte 2-3-C-SOL.asm
@@ -0,0 +1,7 @@
+  ORG 1000H
+A DB ?
+  ORG 2000H
+  mov bx, offset A  ;guardo previamente la direccion de memoria
+  INT 6  ;ingreso caracter
+  HLT
+END
---
+++ (SI105) Aquitectura de Computadoras (AC)/Prácticas/Práctica 1/Parte 2-3-C.asm
@@ -0,0 +1,11 @@
+  ORG 1000H
+A DB ?
+  ORG 2000H
+  int 6
+  mov bx, offset A
+END
+
+;errores:
+;1) falta hlt
+;2) Debo guardar en BX la dirección donde será almecnado el caracter
+; ANTES de pasar por la instrucción INT 6
