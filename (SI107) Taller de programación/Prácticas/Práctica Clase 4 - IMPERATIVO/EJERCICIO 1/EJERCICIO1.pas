fbad0afc0bae6ca5b7de4da598734685ce904def
Franco Matias Dogil <franndogil@gmail.com>
2024-09-08 17:54:31 -0300

    ㅤ

--- (SI107) Taller de programación/Prácticas/Práctica Clase 4 - IMPERATIVO/EJERCICIO 1/EJERCICIO1.pas
+++ (SI107) Taller de programación/Prácticas/Práctica Clase 4 - IMPERATIVO/EJERCICIO 1/EJERCICIO1.pas
@@ -50,14 +50,14 @@ begin
 		a^.HD:=nil;
 	end
 	else begin
-		if(a^.dato.codpv<v.codpv)then
+		if(v.codpv<=a^.dato.codpv)then
 			Agregar(a^.HI, v)
 		else
 			Agregar(a^.HD, v);
 	end;
 end;
-	
-procedure Cargar_ventas(var a:arbol);
+
+procedure Generar_arbol(var a:arbol);		//PUNTO A
 var
 	v:venta;
 begin
@@ -71,13 +71,13 @@ end;
 procedure Imprimir_arbol(a:arbol);
 begin
 	if(a<>nil)then begin
-		Imprimir_arbol(a^.HI);
+		Imprimir_arbol(a^.HD);
 		writeln('-----------------------------------------------');
 		writeln('Codigo de venta: ', a^.dato.codv);
 		writeln('Codigo de producto vendido: ', a^.dato.codpv);
 		writeln('Cantidad de unidades vendidas: ', a^.dato.cantuv);
 		writeln('Precio unitario: ', a^.dato.preu:2:2);
-		Imprimir_arbol(a^.HD);
+		Imprimir_arbol(a^.HI);
 	end;
 end;
 
@@ -101,40 +101,38 @@ begin
 	end;
 end;
 
-procedure Monto_tot(a:arbol; var monto:real; min:integer; max:integer);		//PUNTO E
+function Monto_total(a:arbol; min:integer; max:integer):real;		//PUNTO E
 var
-	montoact:real;
+	monto_act:real;
 begin
-	montoact:=0;
-	if(a<>nil)then begin
-		if(a^.dato.codpv>min)then begin
-			if(a^.dato.codpv<max)then begin
-				montoact:=a^.dato.preu*a^.dato.cantuv;
-				monto:=monto+montoact;
-			end;
+	monto_act:=0;
+	if(a=nil)then
+		Monto_total:=0
+	else begin
+		if(a^.dato.codpv > min)then begin
+			if(a^.dato.codpv < max)then begin
+				monto_act:=a^.dato.cantuv*a^.dato.preu;
+				Monto_total:=monto_act + Monto_total(a^.HI, min, max) + Monto_total(a^.HD, min, max);
+			end
+			else
+				Monto_total:=Monto_total(a^.HI, min, max);
 		end
-		else begin
-			if(a^.dato.codpv>max)then
-				Monto_tot(a^.HD, monto, min, max);
-			if(a^.dato.codpv<min)then
-				Monto_tot(a^.HI, monto, min, max);
-		end;
+		else
+			Monto_total:=Monto_total(a^.HD, min, max);
 	end;
 end;
 
 var
 	a:arbol;
 	mencod, codpro, cantcodpro, min, max:integer;
-	monto:real;
 begin
 	randomize;
 	a:=nil;
 	mencod:=9999;
 	cantcodpro:=0;
-	monto:=0;
 	min:=9999;
 	max:=-1;
-	Cargar_ventas(a);	//PUNTO A
+	Generar_arbol(a);	//PUNTO A
 	Imprimir_arbol(a);	//PUNTO B
 	
 	Men_cod_pro(a, mencod);	//PUNTO C
@@ -145,11 +143,8 @@ begin
 	Cant_menores(a, codpro, cantcodpro);	//PUNTO D
 	writeln('Cantidad de codigos menores que el codigo ingresado: ', cantcodpro);
 	
-	writeln('Ingrese el codigo de producto minimo: ');		//PUNTO E
-	readln(min);												//PUNTO E
-	writeln('Ingrese el codigo de producto maximo: ');		//PUNTO E
-	readln(max);												//PUNTO E
-	Monto_tot(a, monto, min, max);								//PUNTO E
-	writeln('Monto total vendido entre el producto min y el producto max: ', monto:2:2);	//PUNTO E
+	min:=5;
+	max:=20;								
+	writeln('Monto total vendido entre el producto min y el producto max: ', Monto_total(a, min, max):2:2);	//PUNTO E
 end.
 
