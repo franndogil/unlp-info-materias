523dab709547b0be3ffa6ef1eb91f09b22208f30
Franco Matias Dogil <franndogil@gmail.com>
2024-09-27 19:14:47 -0300

    ㅤ

--- (SI107) Taller de programación/Prácticas/OBJETOS/Práctica Clase 1 - OBJETOS/TPProyectoAlumnos/TP_ProyectoAlumnos/src/tema2/Ej04Casting.java
+++ (SI107) Taller de programación/Prácticas/OBJETOS/Práctica Clase 1 - OBJETOS/TPProyectoAlumnos/TP_ProyectoAlumnos/src/tema2/Ej04Casting.java
@@ -1,24 +1,21 @@
 /*
-3- Se realizará un casting para un programa de TV. El casting durará a lo sumo 5 días y en
-cada día se entrevistarán a 8 personas en distinto turno.
-a) Simular el proceso de inscripción de personas al casting. A cada persona se le pide
-nombre, DNI y edad y se la debe asignar en un día y turno de la siguiente manera: las
-personas primero completan el primer día en turnos sucesivos, luego el segundo día y así
-siguiendo. La inscripción finaliza al llegar una persona con nombre “ZZZ” o al cubrirse los
-40 cupos de casting.
+ 4- Sobre un nuevo programa, modifique el ejercicio anterior para considerar que:
+a) Durante el proceso de inscripción se pida a cada persona sus datos (nombre, DNI, edad)
+y el día en que se quiere presentar al casting. La persona debe ser inscripta en ese día, en el
+siguiente turno disponible. En caso de no existir un turno en ese día, informe la situación.
+La inscripción finaliza al llegar una persona con nombre “ZZZ” o al cubrirse los 40 cupos de
+casting.
 Una vez finalizada la inscripción:
-b) Informar para cada día y turno asignado, el nombre de la persona a entrevistar.
-NOTA: utilizar la clase Persona. Pensar en la estructura de datos a utilizar. Para comparar
-Strings use el método equals.
+b) Informar para cada día: la cantidad de inscriptos al casting ese día y el nombre de la
+persona a entrevistar en cada turno asignado.
  */
-
 package tema2;
 
 import PaqueteLectura.GeneradorAleatorio;
 
-public class Ej03Casting {
+public class Ej04Casting {
     public static void main(String[] args) {
-        Persona [][] matrizCasting = new Persona [5][8];
+         Persona [][] matrizCasting = new Persona [5][8];
         
         String nombre = GeneradorAleatorio.generarString(3);
         int edad = GeneradorAleatorio.generarInt(100);
@@ -61,4 +58,5 @@ public class Ej03Casting {
             }
         }
     }
+    
 }
