3e4e67a8a92d2117ae1d35dea5d011e071ff010e
frandogil <franndogil@gmail.com>
2024-10-23 15:53:18 -0300

    ‎ ‎ ‎ ‎ ‎ ‎ ‎

--- (SI105) Aquitectura de Computadoras (AC)/Prácticas/Práctica 4/Parte 2 - 2 - B.s
+++ (SI105) Aquitectura de Computadoras (AC)/Prácticas/Práctica 4/Parte 2 - 2 - B.s
@@ -12,7 +12,7 @@ clavei: .asciiz   "    "
 	ld $t0, CONTROL($zero)
 	ld $t1, DATA($zero)
 
-	daddi $t2, $zero, text1
+inicio:	daddi $t2, $zero, text1
 	sd $t2, 0($t1)
 
 	daddi $t2, $zero, 4
@@ -52,7 +52,7 @@ incorrecta: daddi $t2, $zero, text3
 
 	    daddi $t2, $zero, 4
 	    sd $t2, 0($t0)
-            j fin
+            j inicio
 
 sigue1:	    daddi $t3, $zero, 1
 	    daddi $t6, $zero, 8
