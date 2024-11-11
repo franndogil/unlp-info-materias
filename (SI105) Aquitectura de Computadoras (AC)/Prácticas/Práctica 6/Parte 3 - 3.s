15aedb4fb9cc640d68e2c052c52abf3032f9220c
frandogil <franndogil@gmail.com>
2024-11-11 18:00:46 -0300

    ‎ ‎ ‎ ‎ ‎ ‎ ‎

---
+++ (SI105) Aquitectura de Computadoras (AC)/Prácticas/Práctica 6/Parte 3 - 3.s
@@ -0,0 +1,16 @@
+.data
+A: .word 2, 1, 3, 1, 4, 1
+MAX: .word -1
+.code
+ld $t1, MAX($0)
+daddi $t2, $0, 0
+daddi $t3, $0, 6 
+loop: ld $t4, A($t2)
+slt $t5, $t1, $t4
+beqz $t5, chico
+daddi $t1, $t4, 0
+chico: daddi $t2, $t2, 8
+daddi $t3, $t3, -1
+bnez $t3, loop
+sd $t1, MAX($0)
+halt
