9cc4d9c2d4832591d81e4c1739c9d9137055cf3f
Franco Matias Dogil <franndogil@gmail.com>
2025-03-18 15:41:49 -0300

    ㅤㅤㅤㅤㅤ

---
+++ (SI209) Fundamentos de Organizacion de Datos/Práctica/Practica1/Ej01.pas
@@ -0,0 +1,19 @@
+program ej01;
+type
+	archivo=file of integer;
+var
+	arc_logico:archivo;
+	nro:integer;
+	arc_fisico:string[12];
+begin
+	writeln('Nombre del archivo: ');
+	readln(arc_fisico);
+	assign(arc_logico, arc_fisico);
+	rewrite(arc_logico);
+	read(nro);
+	while(nro<>30000)do begin
+		writle(arc_logico, nro);
+		read(nro):
+	end;
+	close(arc_logico);
+end.
