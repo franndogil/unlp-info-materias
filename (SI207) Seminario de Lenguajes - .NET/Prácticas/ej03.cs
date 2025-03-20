b5154361a76061beeaa5bbec6f76ffd76123091a
Franco Matias Dogil <franndogil@gmail.com>
2025-03-20 10:12:13 -0300

    ㅤㅤㅤㅤㅤ

---
+++ (SI207) Seminario de Lenguajes - .NET/Prácticas/ej03.cs
@@ -0,0 +1,3 @@
+string st = "c:\\windows\\system";
+Console.WriteLine(st);
+//Para solucionar el problema debo agregar la doble barra a la sintaxis.
---
+++ (SI209) Fundamentos de Organizacion de Datos/Práctica/Practica 1/ej04.pas
@@ -0,0 +1,11 @@
+{4. Agregar al menú del programa del ejercicio 3, opciones para: 
+a. Añadir uno o más empleados al final del archivo con sus datos ingresados por 
+teclado. Tener en cuenta que no se debe agregar al archivo un empleado con 
+un número de empleado ya registrado (control de unicidad). 
+b. Modificar la edad de un empleado dado. 
+c. Exportar el contenido del archivo a un archivo de texto llamado 
+“todos_empleados.txt”. 
+d. Exportar a un archivo de texto llamado: “faltaDNIEmpleado.txt”, los empleados 
+que no tengan cargado el DNI (DNI en 00). 
+NOTA: Las búsquedas deben realizarse por número de empleado. }
+
