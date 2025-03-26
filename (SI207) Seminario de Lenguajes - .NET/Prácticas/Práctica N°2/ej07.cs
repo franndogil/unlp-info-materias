90aa5c5141ef6d0f82da3c30fc34f295a8a5020d
Franco Matias Dogil <franndogil@gmail.com>
2025-03-26 14:58:21 -0300

    ㅤㅤㅤㅤㅤ

---
+++ (SI207) Seminario de Lenguajes - .NET/Prácticas/Práctica N°2/ej07.cs
@@ -0,0 +1,12 @@
+﻿//¿Cuál es la salida por consola del siguiente fragmento de código? ¿Por qué la tercera y sexta línea producen resultados diferentes?
+
+ char c1 = 'A';
+ char c2 = 'A';
+ Console.WriteLine(c1 == c2);
+ object o1 = c1;
+ object o2 = c2;
+ Console.WriteLine(o1 == o2);
+
+ Console.ReadKey();
+
+//La salida por consola es TRUE FALSE, la tercera linea compara resultados inmediatos ('A' con 'A')(TRUE), mientras que la sexta linea compara direcciones de memoria (FALSE). 
