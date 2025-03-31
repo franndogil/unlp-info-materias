a8eea18263a8efab648d898bc4f5f5d2e777bcd2
Franco Matias Dogil <franndogil@gmail.com>
2025-03-31 11:54:37 -0300

    ㅤㅤㅤㅤㅤ

---
+++ (SI207) Seminario de Lenguajes - .NET/Prácticas/Práctica N°2/ej13.cs
@@ -0,0 +1,9 @@
+﻿/* 13) ¿Cuál es la salida por consola si no se pasan argumentos por la línea de comandos?*/
+
+    Console.WriteLine(args == null);
+    Console.WriteLine(args.Length);
+
+    Console.ReadKey();
+//Salida por consola:
+//False     aca da falso pero compara null con ningun argumento???
+//0     por cero argumentos pasados en el launch.json
---
+++ (SI207) Seminario de Lenguajes - .NET/Prácticas/Práctica N°2/ej14.cs
@@ -0,0 +1,17 @@
+﻿/*14) ¿Qué hace la instrucción? ¿Asigna a la variable vector el valor null?*/
+
+int[]? vector = new int[0];     //creo un vector con 0 espacios 
+Console.WriteLine(vector);
+
+if(vector == null){
+    Console.WriteLine("Si, apunta a null");
+}
+else{
+    Console.WriteLine("No apunta a null");
+}
+
+Console.ReadKey();
+
+//Por consola imprimo el objeto vector
+
+//La variable vector no es null
