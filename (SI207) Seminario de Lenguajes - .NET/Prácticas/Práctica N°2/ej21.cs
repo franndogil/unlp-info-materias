333a1bfa39e58f5b3afbcf4c690841bd3fe8fc7b
Franco Matias Dogil <franndogil@gmail.com>
2025-03-31 14:15:33 -0300

    ㅤㅤㅤㅤㅤ

---
+++ (SI207) Seminario de Lenguajes - .NET/Prácticas/Práctica N°2/ej21.cs
@@ -0,0 +1,17 @@
+﻿/*21) Codificar el método Imprimir para que el siguiente código produzca la salida por consola que se 
+observa. Considerar que el usuario del método Imprimir podría querer más adelante imprimir otros 
+datos, posiblemente de otros tipos pasando una cantidad distinta de parámetros cada vez que invoque el 
+método. Tip: usar params*/
+
+void imprimir(params object[] parametros){
+    for(int i = 0; i < parametros.Length; i++){
+        Console.Write(parametros[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+imprimir(1, "casa", 'A', 3.4, DayOfWeek.Saturday);
+imprimir(1, 2, "tres");
+imprimir();
+imprimir("-------------");
+Console.ReadKey();
