a56b7dcd1da08b81f5b8fb4604aef933c0d89858
Franco Matias Dogil <franndogil@gmail.com>
2025-04-10 14:11:19 -0300

    ㅤㅤㅤㅤㅤ

--- (SI209) Fundamentos de Organizacion de Datos/Práctica/Práctica 2/ej02/ej02.pas
+++ (SI209) Fundamentos de Organizacion de Datos/Práctica/Práctica 2/ej02/ej02.pas
@@ -48,12 +48,22 @@ begin
 	end;
 end;
 //----------------------------------------------------------
+procedure leerVenta(var ven:venta);
+begin
+	writeln('Ingrese el codigo de producto vendido: ');
+	readln(ven.codigo);
+	if(ven.codigo<>0)then begin
+		writeln('Ingrese la cantidad vendida: ');
+		readln(ven.cant);
+	end;
+end;
+//----------------------------------------------------------
 procedure crearMaestro(var aMaestro:archivoMaestro);
 var
 	nombreArchivo:string;
 	pro:producto;
 begin
-	writeln('Ingrese el nombre del archivo a crear: ');
+	writeln('Ingrese el nombre del archivo maestro a crear: ');
 	readln(nombreArchivo);
 	assign(aMaestro, nombreArchivo);
 	rewrite(aMaestro);
@@ -67,23 +77,98 @@ end;
 //----------------------------------------------------------
 procedure crearDetalle(var aDetalle:archivoDetalle);
 var
-	nom
+	nombreArchivo:string;
+	ven:venta;
 begin
-	
+	writeln('Ingrese el nombre del archivo detalle a crear: ');
+	read(nombreArchivo);
+	assign(aDetalle, nombreArchivo);
+	rewrite(aDetalle);
+	leerVenta(ven);
+	while(ven.codigo<>0)do begin
+		write(aDetalle, ven);
+		leerVenta(ven);
+	end;
+	close(aDetalle);
 end;
 //----------------------------------------------------------
 procedure actualizarMaestro(var aMaestro:archivoMaestro; var aDetalle:archivoDetalle);
+var
+	nombreMaestro, nombreDetalle:string;
+	ven:venta;
+	pro:producto;
 begin
+	writeln('Ingrese el nombre del archivo maestro a actualizar: ');
+	readln(nombreMaestro);
+	writeln('Ingrese el nombre del archivo detalle a utilizar: ');
+	readln(nombreDetalle);
+	
+	assign(aMaestro, nombreMaestro);
+	reset(aMaestro);
+	assign(aDetalle, nombreDetalle);
+	reset(aDetalle);
+	
+	while(not eof(aDetalle))do begin
+		read(aMaestro, pro);
+		read(aDetalle, ven);
+		while(pro.codigo <> ven.codigo)do 		//{Se busca en el maestro el producto del detalle}
+			read(aMaestro, pro);
+		pro.stAct := pro.stAct - ven.cant;		//{Se modifica el stock del producto con la cantidad vendida de ese producto}
+		seek(aMaestro, filepos(aMaestro)-1);	//{Se reubica el puntero en el maestro}
+		write(aMaestro, pro);					//{Se escribe el producto en el maestro}
+	end;
+	close(aMaestro);
+	close(aDetalle);
 end;
 //----------------------------------------------------------
-procedure exportarMaestroText(var aMaestro:archivoMaestro);
+procedure exportarMaestroText(var aMaestro:archivoMaestro; var aText:text);
+var
+	nombreArchivo:string;
+	pro:producto;
 begin
+	writeln('Ingrese el nombre del archivo maestro a exportar: ');
+	readln(nombreArchivo);
+	assign(aMaestro, nombreArchivo);
+	reset(aMaestro);
+	assign(aText, nombreArchivo + '.txt');
+	rewrite(aText);
+	while(not eof(aMaestro))do begin
+		read(aMaestro, pro);
+		with pro do begin
+			writeln(aText, codigo, ' ', nombre, ' ', precio, ' ', stAct, ' ', stMin);
+		end;
+	end;
+	close(aText);
+	close(aMaestro);
+end;
+
+//----------------------------------------------------------
+procedure exportarDetalleText(var aDetalle:archivoDetalle; var aText:text);
+var
+	nombreArchivo:string;
+	ven:venta;
+begin
+	writeln('Ingrese el nombre del archivo maestro a exportar: ');
+	readln(nombreArchivo);
+	assign(aDetalle, nombreArchivo);
+	reset(aDetalle);
+	assign(aText, nombreArchivo + '.txt');
+	rewrite(aText);
+	while(not eof(aDetalle))do begin
+		read(aDetalle, ven);
+		with ven do begin
+			writeln(aText, codigo, ' ', cant);
+		end;
+	end;
+	close(aText);
+	close(aDetalle);
 end;
 //----------------------------------------------------------
 var
 	opciones:char;
 	aMaestro:archivoMaestro;
 	aDetalle:archivoDetalle;
+	aText:text;
 begin
 	repeat
 		writeln('Ingrese una de las siguientes opciones: ');
@@ -91,14 +176,16 @@ begin
 		writeln('2. Crear archivo de ventas.');
 		writeln('3. Actualizar archivo maestro.');
 		writeln('4. Exportar archivo maestro.');
-		writeln('5. Salir.');
+		writeln('5. Exportar archivo detalle.');
+		writeln('6. Salir.');
 		readln(opciones);
 		
 		case opciones of
 			'1': crearMaestro(aMaestro);
 			'2': crearDetalle(aDetalle);
 			'3': actualizarMaestro(aMaestro, aDetalle);
-			'4': exportarMaestroText(aMaestro);
+			'4': exportarMaestroText(aMaestro, aText);
+			'5': exportarDetalleText(aDetalle, aText);
 		end;
-	until (opciones = '5');
+	until (opciones = '6');
 end.
