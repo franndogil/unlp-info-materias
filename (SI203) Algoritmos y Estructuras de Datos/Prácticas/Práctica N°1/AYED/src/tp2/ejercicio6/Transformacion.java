b598199551b82332cbed913116ed8520a5d56ccf
Franco Matias Dogil <franndogil@gmail.com>
2025-04-21 16:53:50 -0300

    ㅤ

--- (SI203) Algoritmos y Estructuras de Datos/Prácticas/Práctica N°1/AYED/src/tp2/ejercicio6/Transformacion.java
+++ (SI203) Algoritmos y Estructuras de Datos/Prácticas/Práctica N°1/AYED/src/tp2/ejercicio6/Transformacion.java
@@ -1,5 +1,31 @@
 package tp2.ejercicio6;
 
-public class Transformacion {
+import tp2.ejercicio1.*;
 
+public class Transformacion {
+	private BinaryTree<Integer> tree;
+	
+	public Transformacion(BinaryTree<Integer> tree) {
+		this.tree = tree;
+	}
+	
+	public BinaryTree<Integer> suma(){
+		sumaArbol(tree);
+		return tree;
+	}	
+	
+	private int sumaArbol(BinaryTree<Integer> tree) {
+		int suma = 0;
+		if(tree.isLeaf()) {
+			suma = tree.getData();
+			tree.setData(0);
+			return suma;
+		}
+		if(tree.hasLeftChild()) suma += sumaArbol(tree.getLeftChild());
+		if(tree.hasRightChild()) suma += sumaArbol(tree.getRightChild());
+		
+		int actual = tree.getData();		
+		tree.setData(suma);
+		return suma + actual;
+	}
 }
