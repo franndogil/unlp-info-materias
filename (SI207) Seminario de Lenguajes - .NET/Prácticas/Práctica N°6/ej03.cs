c31884c27a9a332d4b2bade6cfe72ee58a708e8e
Franco Matias Dogil <franndogil@gmail.com>
2025-04-30 12:35:33 -0300

    ㅤㅤㅤㅤ                                          ㅤ

---
+++ (SI207) Seminario de Lenguajes - .NET/Prácticas/Práctica N°6/ej03.cs
@@ -0,0 +1,12 @@
+﻿class Auto
+ {
+  protected double velocidad;
+  public virtual void Acelerar() => Console.WriteLine("Velocidad = {0}", velocidad += 10);  //faltaba $
+ }
+ class Taxi : Auto
+ {
+  public override void Acelerar() => Console.WriteLine("Velocidad = {0}", velocidad += 5);  //faltaba $
+ }
+
+ //el problema esta en la variable velocidad de Taxi, no podemos acceder debido a su nivel de proteccion
+ //una de las soluciones es declarar velocidad como protected
---
+++ (SI207) Seminario de Lenguajes - .NET/Prácticas/Práctica N°6/ej04.cs
@@ -0,0 +1,29 @@
+﻿/*4) Contestar sobre el siguiente programa:*/
+
+
+Taxi t = new Taxi(3, "Ford");
+Console.WriteLine($"Un {t.Marca} con {t.Pasajeros} pasajeros");
+class Auto
+{
+    public string Marca { get; private set; } = "Ford";
+    public Auto(string marca) => this.Marca = marca;
+ }
+ class Taxi : Auto
+ {
+    public int Pasajeros  { get; private set; }
+    public Taxi(int pasajeros, string marca) : base (marca) => this.Pasajeros = pasajeros;
+ }
+
+/* ¿Por qué no es necesario agregar :base en el constructor de Taxi?
+RTA: Porque en el constructor de la clase Auto nos encontramos con un constructor sin parametros.
+class Auto
+{
+    public string Marca { get; private set; } = "Ford";
+    public Auto(string marca) => this.Marca = marca;
+    public Auto(){
+    }
+ }
+
+
+Eliminar el segundo constructor de la clase Auto y modificar la clase 
+Taxi para el programa siga funcionando.*/
