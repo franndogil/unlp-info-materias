8797c36d19dd37b96ce1f9591bf32c1dad3eb998
Franco Matias Dogil <franndogil@gmail.com>
2025-05-01 12:51:53 -0300

    EJ08 AYED

--- (SI203) Algoritmos y Estructuras de Datos/Prácticas/Práctica N°1/AYED/src/tp3/ejercicio8/Main.java
+++ (SI203) Algoritmos y Estructuras de Datos/Prácticas/Práctica N°1/AYED/src/tp3/ejercicio8/Main.java
@@ -1,7 +1,58 @@
 package tp3.ejercicio8;
 
+import java.util.*;
+import tp3.ejercicio1.*;
+
 public class Main {
 	public static void main(String[] args) {
 		
+		/*/*                 1
+					       / | \
+					      2  3  4				*/
+		
+		List<GeneralTree<Integer>> subArb1 = new LinkedList<GeneralTree<Integer>>();
+        subArb1.add(new GeneralTree<Integer>(2));
+        subArb1.add(new GeneralTree<Integer>(3));
+        subArb1.add(new GeneralTree<Integer>(4));
+        GeneralTree<Integer> a1 = new GeneralTree<Integer>(1, subArb1);
+        
+        /*/*                   1
+	      					 / | \
+	      					2  3  4				
+	      				  / | 
+	      				 5  6  							*/
+        
+        List<GeneralTree<Integer>> subArb2A = new LinkedList<GeneralTree<Integer>>();
+        subArb2A.add(new GeneralTree<Integer>(5));
+        subArb2A.add(new GeneralTree<Integer>(6));
+        //subArb2A.add(new GeneralTree<Integer>(7));
+        //le saco el 7 y ya deja de ser abeto navidenio
+        
+        GeneralTree<Integer> subA2 = new GeneralTree<Integer>(2, subArb2A);
+        List<GeneralTree<Integer>> subArb2B = new LinkedList<GeneralTree<Integer>>();
+        subArb2B.add(subA2);
+        subArb2B.add(new GeneralTree<Integer>(3));
+        subArb2B.add(new GeneralTree<Integer>(4));
+        GeneralTree<Integer> a2 = new GeneralTree<Integer>(1, subArb2B);
+        
+        List<GeneralTree<Integer>> subArb3A = new LinkedList<GeneralTree<Integer>>();
+        subArb3A.add(new GeneralTree<Integer>(6));
+        subArb3A.add(new GeneralTree<Integer>(7));
+        subArb3A.add(new GeneralTree<Integer>(8));
+        GeneralTree<Integer> subA3 = new GeneralTree<Integer>(3, subArb3A);
+        List<GeneralTree<Integer>> subArb3B = new LinkedList<GeneralTree<Integer>>();
+        subArb3B.add(new GeneralTree<Integer>(2));
+        subArb3B.add(subA3);
+        subArb3B.add(new GeneralTree<Integer>(4));
+        subArb3B.add(new GeneralTree<Integer>(5));
+        GeneralTree<Integer> a3 = new GeneralTree<Integer>(1, subArb3B);
+        
+        Navidad nav1 = new Navidad(a1);
+        Navidad nav2 = new Navidad(a2);
+        Navidad nav3 = new Navidad(a3);
+        
+        System.out.println("Es abeto navidenio A1? " + nav1.esAbetoNavidenio());
+        System.out.println("Es abeto navidenio A2? " + nav2.esAbetoNavidenio());
+        System.out.println("Es abeto navidenio A3? " + nav3.esAbetoNavidenio());
 	}
 }
--- (SI203) Algoritmos y Estructuras de Datos/Prácticas/Práctica N°1/AYED/src/tp3/ejercicio8/Navidad.java
+++ (SI203) Algoritmos y Estructuras de Datos/Prácticas/Práctica N°1/AYED/src/tp3/ejercicio8/Navidad.java
@@ -13,6 +13,7 @@ package tp3.ejercicio8;
 
 import tp3.ejercicio1.GeneralTree;
 import tp1.ejercicio8.Queue;
+import java.util.*;
 
 public class Navidad {
 	private GeneralTree<Integer> arbol;
@@ -25,11 +26,39 @@ public class Navidad {
 		if(arbol.isEmpty()) {
 			return "Esta vacio, no es un abeto navidenio.";
 		}
+		boolean abeto = recorridoPorNiveles(arbol);
 		
-		//meto recorrido por niveles ya fue
+		if(abeto){
+			return "Es un abeto navidenio.";
+		}
 		
+		return "No es un abeto navidenio.";
+	}
+	
+	private boolean recorridoPorNiveles(GeneralTree<Integer> arbol) {
+		boolean cond = true;	//es navidenio hasta que se demuestre lo contrario
 		Queue<GeneralTree<Integer>> cola = new Queue<>();
+		GeneralTree<Integer> aux;	//para desencolar
 		
+		cola.enqueue(arbol);		//encolo al primero
+		cola.enqueue(null);			//separo el nivel
 		
+		while(!cola.isEmpty() && cond) {
+			aux = cola.dequeue();	//desencolo al primero en aux
+			if(aux != null) {		//si es distinto de null 
+				if(aux.hasChildren()) {
+					if(aux.getChildren().size()>=3) {
+						List<GeneralTree<Integer>> listAux = aux.getChildren();
+						for(GeneralTree<Integer> a : listAux) {
+							cola.enqueue(a);	//encolo a cada uno de los hijos
+						}
+					}else cond = false;		//si tiene hijos pero no son mayor o igual a tres ya no es un arbol navidenio
+				}
+			}else if(!cola.isEmpty()) {		//si la cola no esta vacia encolamos null para hacer la separacion de nivel
+				cola.enqueue(null);
+			}
+		}
+		
+		return cond;
 	}
 }
